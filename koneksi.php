fe6eb080bd04d2f2e9f0b8d7275c79d1f420a836
jerybablo <suryanto.jeri@gmail.com>
2017-03-02 07:23:07 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+
+//mysql_connect("nama server","nama user","password")
+
+$cek = mysql_connect("localhost","jeri","1234") or die ("database tidak berhasil connect");
+
+/*if ($cek) {
+	echo "Database berhasil connect<br>";
+}
+*/
+?>
